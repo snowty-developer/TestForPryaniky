f539e972950244a65abff8d2652502bba2f09755
snowty-developer <snowty.player@gmail.com>
2021-02-12 18:52:22 +0300

    Initial Commit

---
+++ TestForPryaniky/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestForPryaniky
+//
+//  Created by Александр Зубарев on 12.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
